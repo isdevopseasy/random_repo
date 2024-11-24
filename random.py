569957ff3e61a9afdcac95ffdcef9ea1db89a8a7
canilearndevops <canilearndevops@gmail.com>
2024-11-24 12:52:55 +0530

    addressing a conflict

--- random.py
+++ random.py
@@ -1,5 +1,7 @@
 print("making pushing of code difficult for other developers")
 
 print("will you able to make a push")
-      
+
+print("this line needs to come in between to cause conflicts")
+
 print("however trying to make a push")
